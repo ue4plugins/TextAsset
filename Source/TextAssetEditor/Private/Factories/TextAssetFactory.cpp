492023e86ef8d6171331032c6a76c598f3082f33
Gerke Max Preussner <info@headcrash.us>
2015-12-14 01:32:36 -0500

    Added support for drag & drop of text files.

--- Source/TextAssetEditor/Private/Factories/TextAssetFactory.cpp
+++ Source/TextAssetEditor/Private/Factories/TextAssetFactory.cpp
@@ -10,6 +10,7 @@
 UTextAssetFactory::UTextAssetFactory( const FObjectInitializer& ObjectInitializer )
 	: Super(ObjectInitializer)
 {
+	Formats.Add(FString(TEXT("txt;")) + NSLOCTEXT("UTextAssetFactory", "FormatTxt", "Text File").ToString());
 	SupportedClass = UTextAsset::StaticClass();
 	bCreateNew = false;
 	bEditorImport = true;
@@ -21,5 +22,16 @@ UTextAssetFactory::UTextAssetFactory( const FObjectInitializer& ObjectInitialize
 
 UObject* UTextAssetFactory::FactoryCreateBinary(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, const TCHAR* Type, const uint8*& Buffer, const uint8* BufferEnd, FFeedbackContext* Warn)
 {
-	return NewObject<UTextAsset>(InParent, Class, Name, Flags);
+	UTextAsset* TextAsset = nullptr;
+	FString TextString;
+
+	FEditorDelegates::OnAssetPreImport.Broadcast(this, Class, InParent, Name, Type);
+
+	if (FFileHelper::LoadFileToString(TextString, *CurrentFilename))
+	{
+		TextAsset = NewObject<UTextAsset>(InParent, Class, Name, Flags);
+		TextAsset->Text = FText::FromString(TextString);
+	}
+
+	return TextAsset;
 }
