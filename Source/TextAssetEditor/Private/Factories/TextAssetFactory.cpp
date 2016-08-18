971ec903a17e5e5d9196993816a75f6c697a240a
Gerke Max Preussner <info@headcrash.us>
2016-08-18 13:15:24 -0400

    Fixed copyright notice.

--- Source/TextAssetEditor/Private/Factories/TextAssetFactory.cpp
+++ Source/TextAssetEditor/Private/Factories/TextAssetFactory.cpp
@@ -1,4 +1,4 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+// Copyright 2015 Headcrash Industries LLC. All Rights Reserved.
 
 #include "TextAssetEditorPrivatePCH.h"
 #include "TextAssetFactory.h"
