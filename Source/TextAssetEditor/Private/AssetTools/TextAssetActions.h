8704b7f6ad410b4af962bdfe890e5cfe28c57396
Jerry Swan <swanj21@gmail.com>
2022-01-19 13:52:16 -0500

    Updating comments to better understand what the purpose of the functions are

--- Source/TextAssetEditor/Private/AssetTools/TextAssetActions.h
+++ Source/TextAssetEditor/Private/AssetTools/TextAssetActions.h
@@ -23,16 +23,47 @@ public:
 	 */
 	FTextAssetActions(const TSharedRef<ISlateStyle>& InStyle);
 
-public:
-
 	//~ FAssetTypeActions_Base overrides
 
 	virtual bool CanFilter() override;
+
+	/**
+	 * Get the actions for the passed in InObjects and display them within the MenuBuilder
+	 * @param InObjects Reference to objects that have actions to display
+	 * @param MenuBuilder Menu that will show the available actions
+	 */
 	virtual void GetActions(const TArray<UObject*>& InObjects, FMenuBuilder& MenuBuilder) override;
+
+	/**
+	 * Where in the content browser content menu this asset will show up, using the EAssetTypeCategories enum
+	 * @return Category that the asset belongs to
+	 */
 	virtual uint32 GetCategories() override;
+
+	/**
+	 * Get the name of the asset, will be shown in the thumbnail within the editor
+	 * @return FText for the name of the asset
+	 */
 	virtual FText GetName() const override;
+
+	/**
+	 * Tells the editor which class the asset is. <br />
+	 * <b>This must be implemented so the editor knows which class is to be used</b>
+	 * @return UClass that is supported
+	 */
 	virtual UClass* GetSupportedClass() const override;
+
+	/**
+	 * Get the color to be used in the thumbnail for the asset
+	 * @return FColor to be used
+	 */
 	virtual FColor GetTypeColor() const override;
+
+	/**
+	 * Whether or not the passed in InObjects should display any context menu action
+	 * @param InObjects Reference to objects that will be checked for actions
+	 * @return If any of the InObjects have actions available
+	 */
 	virtual bool HasActions(const TArray<UObject*>& InObjects) const override;
 	virtual void OpenAssetEditor(const TArray<UObject*>& InObjects, TSharedPtr<IToolkitHost> EditWithinLevelEditor = TSharedPtr<IToolkitHost>()) override;
 
